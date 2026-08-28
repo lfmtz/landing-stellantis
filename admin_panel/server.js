4878d8742f4839a2de5aae3bf4a5523b79493019
lfmtz <luis.martinez.autos@gmail.com>
2026-08-28 15:19:46 -0600

    Allow serving parent directory statically to support local previewing of compiled landing pages

--- admin_panel/server.js
+++ admin_panel/server.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '..')));
 
 // Create upload directories if they don't exist
 const baseImgDir = path.join(__dirname, '..', 'imagenes');
